d37515d51e4d2af8db4775527fdea37460676a46
Cormac Lawlor <armedcor@gmail.com>
2019-07-24 11:29:49 +0000

    Set up python file to run Mongo commands

---
+++ mongo.py
@@ -0,0 +1,14 @@
+import pymongo
+import os
+
+MONGODB_URI = os.getenv("MONGO_URI")
+DBS_NAME = "myTestDB"
+COLLECTION_NAME = "myFirstMDB"
+
+def mongo_connect(url):
+    try:
+        conn = pymongo.MongoClient(url)
+        print("Mongo is connected!")
+        return conn
+    except pymongo.errors.ConnectionFailure as e:
+        print("Could not connect to MongoDB: %s") % e
