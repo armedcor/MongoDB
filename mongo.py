9af7b7b36aa48eb66bce17e48cad259d3ebd2aa1
Cormac Lawlor <armedcor@gmail.com>
2019-07-24 14:06:48 +0000

    Added mongo_project

--- mongo.py
+++ mongo.py
@@ -18,12 +18,11 @@ conn = mongo_connect(MONGODB_URI)
 
 coll = conn[DBS_NAME][COLLECTION_NAME]
 
-new_docs = [{'first': 'terry', 'last': 'pratchett', 'dob': '28/04/1948', 'gender': 'm', 'hair_colour': 'not much', 'occupation': 'writer', 'nationality': 'english'}, 
-{'first': 'george', 'last': 'rr martin', 'dob': '20/09/1948', 'gender': 'm', 'hair_colour': 'white', 'occupation': 'writer', 'nationality': 'american'}]
 
-coll.insert_many(new_docs)
 
-documents = coll.find()
+coll.update_many({'nationality': 'american'}, {'$set': {'hair_colour': 'maroon'}})
+
+documents = coll.find({'nationality': 'american'})
 
 for doc in documents:
     print(doc)
